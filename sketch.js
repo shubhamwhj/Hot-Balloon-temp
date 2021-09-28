f6a65972a50fdfd0e28b14052851849ae96f077f
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-09-28 12:25:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,28 +4,23 @@ var height;
 
 function preload(){
    bg =loadImage("Images/cityImage.png");
-   balloonImage1=loadAnimation("Images/HotAirBallon01.png");
-   balloonImage2=loadAnimation("Images/HotAirBallon01.png","Images/HotAirBallon01.png",
-   "Images/HotAirBallon01.png","Images/HotAirBallon02.png","Images/HotAirBallon02.png",
-   "Images/HotAirBallon02.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png");
+   balloonImage1=loadAnimation("Images/HotAirBallon-01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon-01.png","Images/HotAirBallon-01.png",
+   "Images/HotAirBallon-01.png","Images/HotAirBallon-02.png","Images/HotAirBallon-02.png",
+   "Images/HotAirBallon-02.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png");
   }
 
 //Function to set initial environment
 function setup() {
-
-   database=firebase.database();
-
+  database=firebase.database();
   createCanvas(1500,700);
 
-  balloon=createSprite(250,650,150,150);
+  balloon=createSprite(250,650,250,650);
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
 
   var balloonHeight=database.ref('balloon/height');
-  balloonHeight.on("value",readHeight, showError);
-
-
-
+  balloonHeight.on("value",readHeight, console.log("error"));
   textSize(20); 
 }
 
@@ -57,14 +52,16 @@ function draw() {
   stroke("white");
   textSize(25);
   text("**Use arrow keys to move Hot Air Balloon!",40,40);
+
 }
 
- function updateHeight(x,y){
-   database.ref('balloon/height').set({
-     'x': height.x + x ,
-     'y': height.y + y
-   })
- }
+
+function updateHeight(x,y){
+  database.ref('/balloon/height').update({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
+}
 
 
 //CHOOSE THE CORRECT READHEIGHT FUNCTION
